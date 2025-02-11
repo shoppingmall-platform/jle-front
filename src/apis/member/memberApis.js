ab2f8fd71cbd4b23f7d4faf09947079ae9264413
BangMingGing <privatebmk802@naver.com>
2025-02-11 21:51:54 +0900

    fix: Fix error while call apis in hook

--- src/apis/member/memberApis.js
+++ src/apis/member/memberApis.js
@@ -1,8 +1,17 @@
 import { useApi } from '@/apis/index'
-
-const api = useApi()
+import api from '@/apis/indexTest'
 
 export const test = async () => {
+  const api = useApi()
+  try {
+    const response = await api.get('/v1/st/user/apis')
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const testCustom = async () => {
   try {
     const response = await api.get('/v1/st/user/apis')
     console.log(response)
