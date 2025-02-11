9d8a7661416138ed01c0c654c52179a0136cc5f5
BangMingGing <privatebmk802@naver.com>
2025-02-11 21:58:05 +0900

    refactor: Remove origin api codes and replace to new api codes

--- src/apis/member/memberApis.js
+++ src/apis/member/memberApis.js
@@ -1,17 +1,6 @@
-import { useApi } from '@/apis/index'
-import api from '@/apis/indexTest'
+import api from '@/apis/index'
 
 export const test = async () => {
-  const api = useApi()
-  try {
-    const response = await api.get('/v1/st/user/apis')
-    console.log(response)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-export const testCustom = async () => {
   try {
     const response = await api.get('/v1/st/user/apis')
     console.log(response)
