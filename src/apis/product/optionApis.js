96237e11722d587a9aa178e07b33886ec7ee76af
BangMingGing <privatebmk802@naver.com>
2025-03-09 15:11:55 +0900

    chore: Add /product path to optionApis

--- src/apis/product/optionApis.js
+++ src/apis/product/optionApis.js
@@ -2,7 +2,7 @@ import api from '@/apis/index'
 
 export const getOptionList = async (params) => {
   try {
-    const response = await api.get('/v1/option', { params })
+    const response = await api.get('/product/v1/option', { params })
     console.log(response)
     return response.data
   } catch (error) {
@@ -12,7 +12,7 @@ export const getOptionList = async (params) => {
 
 export const registerOption = async (newOption) => {
   try {
-    const response = await api.post('/v1/option', newOption)
+    const response = await api.post('/product/v1/option', newOption)
     console.log(response)
     return response.data
   } catch (error) {
@@ -22,7 +22,7 @@ export const registerOption = async (newOption) => {
 
 export const deleteOption = async (deleteOptionBody) => {
   try {
-    const response = await api.post('/v1/option/delete-option', deleteOptionBody)
+    const response = await api.post('/product/v1/option/delete-option', deleteOptionBody)
     console.log(response)
     return response.data
   } catch (error) {
