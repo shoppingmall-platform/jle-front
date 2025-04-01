0381b5357c4a8ce5e1c0aa2fa238d785a87945c3
BangMingGing <privatebmk802@naver.com>
2025-04-02 00:18:51 +0900

    feat: 카테고리별 상품 정보 조회 API, 상품 상세 정보 조회 API 추가

--- src/apis/product/productApis.js
+++ src/apis/product/productApis.js
@@ -10,6 +10,26 @@ export const getProductList = async (params = { page: 0, size: 10 }) => {
   }
 }
 
+export const getCategoryProductList = async (categoryId, params = { page: 0, size: 10 }) => {
+  try {
+    const response = await api.get(`/product/v1/${categoryId}/products`, { params })
+    console.log(response)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getProductDetail = async (productId) => {
+  try {
+    const response = await api.get(`/product/v1/products/${productId}`)
+    console.log(response)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export const registerProduct = async (newProduct) => {
   try {
     const response = await api.post('/product/v1/products', newProduct)
@@ -30,4 +50,10 @@ export const updateProduct = async (updateProduct) => {
   }
 }
 
-export default { getProductList, registerProduct, updateProduct }
+export default {
+  getProductList,
+  getCategoryProductList,
+  getProductDetail,
+  registerProduct,
+  updateProduct,
+}
