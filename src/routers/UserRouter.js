bc6df17b5e4ef21949265f55e30d0757f68a418f
BangMingGing <privatebmk802@naver.com>
2025-04-30 00:52:35 +0900

    feat: Header 네비게이션 바에 Tag 버튼 추가, Tag 상품 목록 조회 화면 구현 완료

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 const Home = React.lazy(() => import('@/pages/userPages/Home'))
 const Category = React.lazy(() => import('@/pages/userPages/Category'))
+const Tag = React.lazy(() => import('@/pages/userPages/Tag'))
 const ProductDetail = React.lazy(() => import('@/pages/userPages/ProductDetail'))
 const Login = React.lazy(() => import('@/pages/userPages/Login'))
 const Signup = React.lazy(() => import('@/pages/userPages/Signup'))
@@ -14,6 +15,7 @@ const AddressAdd = React.lazy(() => import('@/pages/userPages/mypage/address/Add
 export const userRouter = [
   { path: '/', element: Home },
   { path: '/category/:categoryId', element: Category },
+  { path: '/tag/:tagId', element: Tag },
   { path: '/product/:productId', element: ProductDetail },
   { path: '/login', element: Login },
   { path: '/signup', element: Signup },
